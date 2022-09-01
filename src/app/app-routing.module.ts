47f00cfa013c4031d0a23522d5b8c14488b6268b
Tzappiello <tzappiello@gmail.com>
2022-09-01 17:14:14 -0300

    Criação do caminha para chamada de lazyLoad admin/home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./core/components/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'admin',
+    loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/components/auth/auth-routing.module.ts
+++ src/app/core/components/auth/auth-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignComponent } from './pages/sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: SignComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
