0e71f4b98475b63b9049bc9ecf5a4604a985c4b1
Tzappiello <tzappiello@gmail.com>
2022-09-01 16:31:26 -0300

    Criação de componentes e ajuste de CSS, carregando as imagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/admin/pages/home/home.component';
+import { AuthComponent } from './core/components/pages/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
